cb7fb5ce1588933f31330cd5f77791cbcafe738b
Yashwanth Sai Sollu <www.sai1929@gmail.com>
2024-10-03 23:58:23 +0530

    Getting Rusty, Intialized Dependencies

---
+++ rust-trading-bot/target/debug/deps/rust_trading_bot-985642f9665622b3.d
@@ -0,0 +1,5 @@
+/Users/yashwanth/Trading_Bot/rust-trading-bot/target/debug/deps/rust_trading_bot-985642f9665622b3: src/main.rs
+
+/Users/yashwanth/Trading_Bot/rust-trading-bot/target/debug/deps/rust_trading_bot-985642f9665622b3.d: src/main.rs
+
+src/main.rs:
---
+++ rust-trading-bot/target/debug/rust-trading-bot.d
@@ -0,0 +1,1 @@
+/Users/yashwanth/Trading_Bot/rust-trading-bot/target/debug/rust-trading-bot: /Users/yashwanth/Trading_Bot/rust-trading-bot/src/main.rs
